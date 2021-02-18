0b3558ad3eec80f8ee49934a274f7e37e3de9ff7
ARSHAN ASLAM KPO <521236@sg.pk>
2021-02-18 17:05:23 +0500

    update contant

---
+++ Jannat_Textile_website/app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("/index.html")
+
+@app.route('/contactus')
+def contactus():
+    return render_template("/contactus.html")
+
+@app.route('/manufacturing')
+def manufacturing():
+    return render_template("/manufacturing.html")
+
+@app.route('/products')
+def products():
+    return render_template("/products.html")
+
+@app.route('/profile')
+def profile():
+    return render_template("/profile.html")
+
+@app.route('/quality-assurance')
+def quality():
+    return render_template("/quality-assurance.html")
+
+
+
+
+if __name__ == '__main__':
+    app.run()
